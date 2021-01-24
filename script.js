7a2d81d078e0ce7bb7e1d59b16075ba67aa9efa7
Olivia Owen <olivia.owen@hotmail.com>
2021-01-24 21:05:38 +0000

    Added weather images and clauses to the js file

--- script.js
+++ script.js
@@ -15,24 +15,86 @@ function citySearch(cityName) {
         url: queryURL,
         method: 'GET'
     })
-    // The .then function means that the following function will not run until the above has been carried out.
-    // In this case meaning it wont carry out the function until the data has loaded from the API requested
-    .then(function (response) {
-        console.log(response);
-        console.log(queryURL);
-        //    The line below ensures that the field is empty before we enter the requested data
-        $("#weatherToday").empty();
-        // using moment.js this will allow us to display the current date
-        var mainDate = moment().format('LL');
+        // The .then function means that the following function will not run until the above has been carried out.
+        // In this case meaning it wont carry out the function until the data has loaded from the API requested
+        .then(function (response) {
+            console.log(response);
+            console.log(queryURL);
+            //    The line below ensures that the field is empty before we enter the requested data
+            $("#weatherToday").empty();
+            // using moment.js this will allow us to display the current date
+            var mainDate = moment().format('LL');
 
 
-        // We need to print the above information on the page within the main container
-        // the below is asking the city name to display in text, in a h3 tag. It will display the city name as the API responds it as well as displaying the main date from the variable.
-        var displayMainDate = cityNameDisplay.append(" - " + mainDate);
-        // The below all display the API response after the text we ask it to. It will display this within a paragraph as we have asked it to. 
-        var tempDisplay = $("<p>").text("Temperature: " + response.main.temp);
-        var humidDisplay = $("<p>").text("Humidity: " + response.main.humidity);
-        var windDisplay = $("<p>").text("Wind Speed: " + response.wind.speed);
-        var currentweather = response.weather[0].main;
+            // We need to print the above information on the page within the main container
+            // the below is asking the city name to display in text, in a h3 tag. It will display the city name as the API responds it as well as displaying the main date from the variable.
+            var cityNameDisplay = $("<h3>").text(response.name);
+            var displayMainDate = cityNameDisplay.append(" - " + mainDate);
+            // The below all display the API response after the text we ask it to. It will display this within a paragraph as we have asked it to.
+            var tempDisplay = $("<p>").text("Temperature: " + response.main.temp);
+            var humidDisplay = $("<p>").text("Humidity: " + response.main.humidity);
+            var windDisplay = $("<p>").text("Wind Speed: " + response.wind.speed);
+            var currentWeather = response.weather[0].main;
 
-        
+            // The below is for the UV index - we have to call this again using a different API link
+            var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?&appid=43833fd258805487a99a9cb6665dbbce" + lat  + "&lon=" + lon;
+            // the below variables are taken from the location API response
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+
+
+            $.ajax({
+                url: queryURLUV,
+                method: 'GET'
+            }).then(function (response) {
+                //    The line below ensures that the field is empty before we enter the requested data
+                $('#uvlDisplay').empty();
+                var uvResults = response.value;
+                
+                var uvDisplay = $("<button class='btn bg-success'>").text("UV Index: " + response.value);
+
+                $('#uvlDisplay').html(uvDisplay);
+
+            });
+
+// the below uses if and else statements to determine what the weather is and displays the appropriate symbol
+            if (currentWeather === "Rain") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            } 
+            else if (currentWeather === "Drizzle") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            else if (currentWeather === "Clouds") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            else if (currentWeather === "Clear") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            else if (currentWeather === "Snow") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/11d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            else if (currentWeather === "Thunderstorm") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            else if (currentWeather === "Mist") {
+                var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/50d.png");
+                currentIcon.attr("style", "height: 60px; width: 60px");
+            }
+            //create HTML div to append new elements to render on page....
+            var newDiv = $('<div>');
+
+            newDiv.append(displayMainDate, currentIcon, tempDisplay, humidDisplay, windDisplay);
+
+            $("#weatherToday").html(newDiv);
+
+            //--------------------------------------------- UV call ---------------------------------------//
+
+
+
+        });
